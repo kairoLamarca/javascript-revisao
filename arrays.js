af3238a7adee9e9b2fe13696cc0e89323d7099a3
kairoLamarca <lamarca.kairo@gmail.com>
2018-02-01 15:26:41 -0200

    Array
    Array

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var lista_compras = Array();
+
+lista_compras[1] = 'Arroz';
+lista_compras[2] = 'Feijão';
+lista_compras[3] = 'Macarrão';
+lista_compras[4] = 'Abacaxi';
+lista_compras[5] = 'Refrigerante';
+
+document.write(lista_compras[3]);
+
+var lista_compras2 = ['Arroz', 'Feijão', 'Macarrão', 'Abacaxi', 'Refrigerante'];
+
+document.write(lista_compras2[3]);
