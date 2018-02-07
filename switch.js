247c52650d14294a9101c380a2108d5db066268d
kairoLamarca <lamarca.kairo@gmail.com>
2018-02-07 14:16:25 -0200

    switch
    switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+var opcao = prompt('Digite uma opção');
+
+switch(parseInt(opcao)){
+
+	case 1:
+  	document.write('Opção 1');
+    break;
+	case 2:
+  	document.write('Opção 2');
+    break;
+  default:
+  	document.write('Opção inválida');
+    
+}
